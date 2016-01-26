0b06fb330870c4584b9c8173b756e099012c1153
SaidSikira <saidsikira@gmail.com>
2016-01-26 19:17:50 +0100

    Added initial docs

---
+++ serve.sh
@@ -0,0 +1,7 @@
+if [ -d CoreCityOS ]; then
+  git -C CoreCityOS pull origin master
+else
+  git clone --depth 1 --branch master https://github.com/cityos/CoreCityOS.git
+fi
+
+jazzy --clean --output master --module-version master
