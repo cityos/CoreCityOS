501d348903d9c5ae84f78246337448dd1b3c281f
SaidSikira <saidsikira@gmail.com>
2016-01-26 20:46:36 +0100

    Added README.md

---
+++ README.md
@@ -0,0 +1,11 @@
+## CoreCityOS Documentation Reference
+
+To build latest documentation you need `jazzy`. To install it run:
+```shell
+sudo gem install jazzy
+```
+After that run:
+```shell
+sh serve.sh
+```
+This will build documentation in folder master.
--- serve.sh
+++ serve.sh
@@ -1,8 +1,10 @@
+# Fetch newest version from Github
 if [ -d CoreCityOS ]; then
   git -C CoreCityOS pull origin master
 else
   git clone --depth 1 --branch master https://github.com/cityos/CoreCityOS.git
 fi
 
+# Generate docs in folder master
 jazzy --clean --output master --module-version master --theme fullwidth --head "$(cat head.html)"
 
