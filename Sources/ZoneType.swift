1c58fd9399e4d1e7a9ff112e6dbcab9e2c000c24
SaidSikira <saidsikira@gmail.com>
2016-02-09 21:32:29 +0100

    Added Zone managment
    New ZoneType protocol and documentation

---
+++ Sources/ZoneType.swift
@@ -0,0 +1,56 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the cityos.io CoreCityOS open source project
+//
+// Copyright (c) 2016 CityOS LLC
+// Licensed under Apache License v2.0
+//
+// See https://cityos.io/ios/LICENCE.txt for license information
+//
+//===----------------------------------------------------------------------===//
+
+import class Foundation.NSDate
+
+/**
+ Structures that adopt this protocol can be used to represent geographical zones that 
+ encapsulate all devices in that specifics zone. Devices can be members of more than 
+ one zone.
+ 
+ Zones are useful when you want to read aggregated data from devices, or when you want
+ to perform specific operation on all devices that the zone manages.
+ 
+ All devices are
+ */
+public protocol ZoneType {
+    
+    /// ID of the zone
+    var zoneID: String { get }
+    
+    /// Name of the zone
+    var name: String { get }
+    
+    /// Creation date of the zone
+    var creationDate: NSDate? { get }
+    
+    /// All devices that zone manages
+    var devices: [DeviceType] { get set }
+}
+
+extension ZoneType {
+    
+    /// Computes Set of the all device IDs from `devices` array that the zone manages
+    public var deviceIDs: Set<String> {
+        let allDeviceIDs = self.devices.map { $0.deviceData.deviceID }
+        return Set<String>(allDeviceIDs)
+    }
+}
+
+//MARK: Custom subscript implementation
+extension ZoneType {
+    
+    public subscript(deviceID: String) -> DeviceType? {
+        return self.devices
+            .filter {$0.deviceData.deviceID == deviceID }
+            .first
+    }
+}
